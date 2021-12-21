956742ed072a3788d7fc05175eb10ea9d673294d
wawanneutron <wawanneutron1331@gmail.com>
2021-12-21 10:22:34 +0700

    login fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
                 <a class="navbar-brand page-scroll" href="#">
-                    <img src="{{ url('assets/img/chackra.jpg') }}" width="60" alt="" />
+                    <img src="{{ url('assets/img/chackra.jpg') }}" alt="cakra" title="PT CHAKRA GAHANA GEMILANG" width="60" alt="" />
                 </a>
             </div>
             <ul class="navbar-nav mr-auto d-none d-sm-block d-lg-block">
@@ -25,23 +25,20 @@
                         Hii Admin yuk kelola dengan mudah<br />
                         data penjualan mu
                     </h1>
-                    <img src="{{ url('assets/img/login_img.png') }}" alt="" class="w-75 d-none d-sm-flex" />
+                    <img src="{{ url('assets/img/login_img.png') }}" alt="cakra" title="PT CHAKRA GAHANA GEMILANG" class="w-75 d-none d-sm-flex" />
                 </div>
                 <div class="section-right col-12 col-md-4">
                     <div class="card">
                         <div class="card-body">
-                            <div class="text-center">
-                                <img src="{{ url('assets/img/chackra.jpg') }}" width="40" alt="">
-                            </div>
                             <form action="{{ route('login') }}" method="POST">
                                 @csrf
                                 <div class="form-group">
                                     <label for="inputEmail">Email address</label>
                                     <input type="email" name="email" value="{{ old('email') }}"
-                                        class="form-control @error('email') is-invalid @enderror" required
+                                        class="form-control @error('email') is-invalid @enderror" tabindex="1" required autofocus 
                                         placeholder="Masukkan Alamat Email">
                                     @error('email')
-                                        <div class="alert alert-danger mt-2">
+                                        <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
                                     @enderror
@@ -49,24 +46,24 @@
                                 <div class="form-group">
                                     <label for="inputPassword">Password</label>
                                     <input type="password" name="password"
-                                        class="form-control @error('password') is-invalid @enderror"
+                                        class="form-control @error('password') is-invalid @enderror" tabindex="2"
                                         placeholder="Masukkan Password">
                                     @error('password')
-                                        <div class="alert alert-danger mt-2">
+                                        <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
                                     @enderror
                                 </div>
                                 <div class="form-group form-check">
-                                    <input type="checkbox" class="form-check-input" id="rememberMe" />
-                                    <label for="rememberMe" class="form-check-label">Remember Me</label>
+                                    <input type="checkbox" name="remember" class="form-check-input" id="rememberMe"  tabindex="3"/>
+                                    <label for="rememberMe" class="form-check-label">Ingatkan Saya</label>
                                 </div>
-                                <button type="submit" class="btn btn-login btn-block">
+                                <button type="submit" class="btn btn-login btn-block"  tabindex="4">
                                     Sign In
                                 </button>
-                                <p class="text-center mt-4">
+                                {{-- <p class="text-center mt-4">
                                     <a href="/forgot-password">I forgot the password</a>
-                                </p>
+                                </p> --}}
                             </form>
                         </div>
                     </div>
