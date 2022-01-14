7d3e1cc5957b32b6af8104c838a77bbff46f3ab2
wawanneutron <wawanneutron1331@gmail.com>
2022-01-14 11:40:00 +0700

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container my-auto">
         <div class="row">
             <div class="col-12 text-center">
-                <span class="pt-4 mb-2">2021 Copyright Store. All Rights Reserved.</span>
+                <span class="pt-4 mb-2" style="font-size: 14px;">Copyright &copy; 2021 &dash; {{ date('Y') }} | PT Chakra Gahana Gemilang</span>
             </div>
         </div>
     </div>
